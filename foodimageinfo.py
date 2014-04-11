9097a815428366e065d4276aabd502fa7d2b174c
enthalpy-yan <enthalpy.yan@gmail.com>
2014-04-11 18:56:00 -0400

    add function for getting food image infos

---
+++ foodimageinfo.py
@@ -0,0 +1,47 @@
+"""
+Module for getting image file from ayahoo_test_images folder.
+
+Use images_iter function to get all files infomation in given directory.
+Use filter_by_attrs function to categorize image by their attribute.
+"""
+
+import os
+import re
+from itertools import imap
+
+def images_iter(directory):
+    """
+    Get a iterator that contains instances of jpg file info dict.
+
+    Parameters
+    ----------
+    directory: full path of a directory
+
+    Returns
+    -------
+    A iterator of a list of file info dict.
+    """
+    def _imagefiles():
+        "Get all of image files from given directory."
+        for subdir in os.listdir(directory):
+            subdir_path = os.path.join(os.path.abspath(directory), subdir)
+            for f in os.listdir(subdir_path):
+                yield os.path.join(os.path.abspath(subdir_path), f)
+
+    def _parse_attribute(filepath):
+        "Get image attribute from the path of image file."
+        filepath = filepath
+        description = re.match(r'(.*)\.jpg',
+                               os.path.basename(filepath)).group(1)
+        businessid = re.match(r'.*/(.*)$',
+                              os.path.dirname(filepath)).group(1)
+        return [filepath, description, businessid]
+
+    def _jpg_file_info(attrlist):
+        """
+        Function for storing jpeg file infos.
+        """
+        attrs = ["path", "description", "businessid"]
+        return dict(zip(attrs, attrlist))
+
+    return (_jpg_file_info(_parse_attribute(f)) for f in _imagefiles())
