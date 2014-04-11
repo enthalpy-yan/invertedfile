326f88931cfba0b63f659dfeabb2be012c3e46f7
enthalpy-yan <enthalpy.yan@gmail.com>
2014-04-11 14:27:27 -0400

    add hamming matchs function

---
+++ tfidf.py
@@ -0,0 +1,25 @@
+"""
+Module for TF-IDF
+"""
+
+from itertools import imap
+from scipy import spatial
+
+def hmatchs(u_iter, v):
+    """
+    Calculate hamming distance between the input binary string and string
+    iterator.
+
+    Parameters
+    ----------
+    u_iter: sets of binary string.
+    v: input binary string.
+
+    Returns
+    -------
+    A sorted hamming distance list.
+    [(dist, binary string from original iterator), ....]
+    """
+    def _get_dist(target):
+        return spatial.distance.hamming(target, v), target
+    return sorted(imap(_get_dist, u_iter))
