d0ed8acfd2d93c16634a2edb1e2b23d568a13511
MinhuiGu <guminhui@hotmail.com>
2014-04-13 13:47:59 -0400

    modified:   tf_idf.py

--- tf_idf.py
+++ tf_idf.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import vq
 
 def getidf(arr):
     k,l = arr.shape
@@ -7,7 +8,11 @@ def getidf(arr):
     j = 0
     for i in arr:
         idf.append(math.log10( l/sum([1 for p in i if p>0])))
-    return idf
+        
+    #return idf
+    
+    #change for small K !!!!!!
+    return [1 for i in range(k)]
 
 def tf_idf(txt,k):
     f = open(txt,'r')
@@ -30,5 +35,21 @@ def tf_idf(txt,k):
 
     return idf,nparr.T
 
+def tfidf_rank(img,code_book,tfidf_arr,idf):
+    codebook = vq.code_book("nomeaning", "Mixed",code_book,K=20,save=True, read_from_txt=True)
+    hist = vq.quatization(img, codebook, soft=False).values()
+    tf = [ float(i)/sum(hist) for i in hist]
+    tfidf = [ x*y for (x,y) in zip(tf,idf)]
+    k,l = tfidf_arr.shape
+    for i in range(k):
+        tfidf_arr[i] = [ j*tfidf[i] for j in tfidf_arr[i]]
+    r = []
+    for i in range(l):
+        r.append(sum(tfidf_arr.T[i]))
+    rank = {x:y for x,y in zip([z for z in range(l)],r)}
+    rank_imgs = [i for (i,j) in sorted(rank.items(), key=lambda x: x[1])][::-1]
+
+    
 idf,ndarr = tf_idf("/Users/minhuigu/Desktop/tfidf/Multiclass_samples.txt",20)
-#print idf,arr
+print tfidf_rank("/Users/minhuigu/Desktop/598/pic.jpeg","/Users/minhuigu/Desktop/Multi_class_codebook.txt",ndarr,idf)
+
